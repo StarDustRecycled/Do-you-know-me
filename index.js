d0d5943454da0c83f5c11072d2e1a6a1540bf76e
Arindam Bhattacharjee <46712930+StarDustRecycled@users.noreply.github.com>
2022-12-08 17:56:13 +0000

    feat: added the greeting

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const chalkAnimation = require("chalk-animation");
 const chalk = require('chalk');
 const CFonts = require('cfonts');
 var score=0;
+var welcomeMsg = chalk.hex('#844b93')(`Hey There !
+What can I call you ?\n`);
+var userName = readlineSync.question(welcomeMsg);
+console.log(chalk.hex('#0be881')("\n------------------------------------------------------------"));
+greet(userName);
+console.log(chalk.hex('#0be881')("------------------------------------------------------------"));
+
+console.log(chalk.hex('#0be881')(`Let's see how much do you know about Arindam ❓ 👀\n`));
